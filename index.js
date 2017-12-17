b25d1453eadedde63618aed45470ec2032646494
Lauren <laurenmontenegro10@gmail.com>
2017-12-17 12:36:45 -0600

    Changed names

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app
-  .get("/tarea", (req, res) => {
+  .get("/task", (req, res) => {
     res.send({ content: "Soy steven" });
   })
   .get("/facebook", (req, res) => {
